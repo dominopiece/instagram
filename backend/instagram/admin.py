b9515a0d662ebe6e6a55bebb09ee7820ae35e8b2
domino <dominopiece@gmail.com>
2022-12-15 05:38:30 +0900

    instagram model 초기 설계

--- backend/instagram/admin.py
+++ backend/instagram/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+# admin 등록 및 커스텀 환경 구축
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    pass
--- backend/instagram/models.py
+++ backend/instagram/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True     # 테이플 미 생성
+
+class Post(TimeStampedModel):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name="my_post_set", on_delete=models.CASCADE
+    )
+    photo = models.ImageField(upload_to="instagram/post/%Y/%m/%d")
+    caption = models.CharField(max_length=500)
+    # tag_set = models.ManyToManyField("Tag", blank=True)
+    location = models.CharField(max_length=100)
+    like_user_set = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, blank=True, related_name="like_post_set"
+    )
+
+class Comment(TimeStampedModel):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    message = models.TextField()
+
+class Tag(TimeStampedModel):
+    name = models.CharField(max_length=50, unique=True)
