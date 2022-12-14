fe250e948dbbcd06c53b99359cf9c67dca87dc18
domino <dominopiece@gmail.com>
2022-12-15 05:43:07 +0900

    instagram admin setup

--- backend/instagram/admin.py
+++ backend/instagram/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment, Tag
 
 # admin 등록 및 커스텀 환경 구축
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
     pass
