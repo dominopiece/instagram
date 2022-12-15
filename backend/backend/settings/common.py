2120b14414c233b24b150ff533e72d6a64c710c6
domino <dominopiece@gmail.com>
2022-12-15 21:20:42 +0900

    restframework 인증 초기 설정

--- backend/backend/settings/common.py
+++ backend/backend/settings/common.py
@@ -137,3 +137,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# 인증 클래스 추가 - 인증된 사용자만 접근가능
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
+}
--- backend/instagram/views.py
+++ backend/instagram/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .models import Post
 from .serializers import PostSerializer
+from rest_framework.permissions import AllowAny
 
 # CRUD API 
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [AllowAny] #FIXME: 인증 적용  
