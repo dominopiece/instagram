4700e4ea42f41118b39bc3313d77877c5e81097a
domino <dominopiece@gmail.com>
2022-12-25 11:09:45 +0900

    회원 가입 폼

--- frontend/src/pages/accounts/Signup.js
+++ frontend/src/pages/accounts/Signup.js
@@ -1,24 +1,49 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from 'react-router-dom';
+import { Alert } from "antd";
 import Axios from "axios";
 
 export default function Signup() {
-    const [inputs, setInputs] = useState({});
+    const [inputs, setInputs] = useState({ username: "", password: ""});
+    const [errors, setErrors] = useState({});
+    const [formDisabled, setFormDisabled] = useState(true);
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
 
     const onSubmit = (e) => {
         e.preventDefault(); 
 
+        setErrors({})
+        setLoading(true)
+
         Axios.post("http://localhost:8000/accounts/signup/", inputs)
         .then(response => {
             console.log("response", response)
+            navigate('/accounts/login');
         })
         .catch(error => {
-           console.log("error", error) 
+           console.log("error", error);
+            if (error.response) {
+                console.log("error response", error.response)
+                setErrors({
+                    username: (error.response.data.username || []).join(" "),
+                    password: (error.response.data.password || []).join(" "),
+                })
+            }
+        })
+        .finally(() => {
+            setLoading=false;
         })
         console.log("onSubmit", inputs)
     }
 
+    // useEffect(() => {
+    //     console.log("changed inputs", inputs);
+    // }, [inputs])
+
     useEffect(() => {
-        console.log("changed inputs", inputs);
+        const isEnabled = Object.values(inputs).every(s => s.length > 0);
+        setFormDisabled(!isEnabled);
     }, [inputs])
 
     const onChange = e => {
@@ -30,10 +55,19 @@ export default function Signup() {
     }
     return (
     <div>
+        {JSON.stringify(errors)}
         <form onSubmit={onSubmit}>
+            <div>
             <input type="text" name="username" onChange={onChange} />
+            {errors.username && <Alert type="error" message={errors.username} />}
+            </div>
+            <div>
             <input type="password" name="password" onChange={onChange}/>
-            <input type="submit" value="회원가입" />
+            {errors.password && <Alert type="error" message={errors.password} />}
+            </div>
+            <div>
+            <input type="submit" value="회원가입" disabled={loading || formDisabled} />
+            </div>
         </form>
     </div>
     )
